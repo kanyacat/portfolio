0c9393cb8bdebf0bb1fcf623952c5df625453858
kan <kanyakancat@gmail.com>
2024-02-24 18:35:10 +0300

    links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,12 +10,32 @@ export default function Home() {
 		<div className={styles.wrapper}>
 			<Container>
 				<div className={styles.content}>
-					<h1>Имя фамилия</h1>
-					<p>привет! я не умею подбирать шрифты лол</p>
-					<p> [здесь будут описания кто я вообще такая когда я это придумаю]</p>
-					<GitHubIcon />
-					<GmailIcon />
-					<WhatsappIcon />
+					<h1>Колесникова Анастасия</h1>
+					<h2>Junior frontend developer</h2>
+					<div className={styles.info}>
+						{' '}
+						<p>привет! я не умею подбирать шрифты лол</p>
+						<p>
+							[здесь будут описания кто я вообще такая когда я это придумаю]
+						</p>
+					</div>
+					<ul className={styles.social}>
+						<li>
+							<a href='https://github.com/kanyacat' target={'_blank'}>
+								<GitHubIcon />
+							</a>
+						</li>
+						<li>
+							<a href='mailto:kanyakancat@gmail.com'>
+								<GmailIcon />
+							</a>
+						</li>
+						<li>
+							<a href='tel:+79520613853'>
+								<WhatsappIcon />
+							</a>
+						</li>
+					</ul>
 				</div>
 			</Container>
 		</div>
