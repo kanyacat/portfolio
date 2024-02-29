2727924b30487b9152d8f94e025fe226ec8b513e
kan <kanyakancat@gmail.com>
2024-02-29 20:54:40 +0300

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import styles from './page.module.scss'
 import Project from '@/components/Project/Project'
 import Main from '@/components/Main/Main'
 import axios from 'axios'
+import { ProjectProps } from '@/components/Project/Project.props'
 
 export default async function Home() {
 	const data = await getProjects()
@@ -9,14 +10,14 @@ export default async function Home() {
 	return (
 		<div className={styles.root}>
 			<Main />
-			{data?.projects.map((p: any) => <Project {...p} key={p.id} />)}
+			{data?.projects.map((p: ProjectProps) => <Project {...p} key={p.id} />)}
 		</div>
 	)
 }
 
-export async function getProjects() {
+async function getProjects() {
 	try {
-		const res = await axios.get(
+		const res = await axios.get<ProjectProps[]>(
 			'https://65d6fa3727d9a3bc1d79cd57.mockapi.io/api/portfolio?orderBy=id&order=desc'
 		)
 
